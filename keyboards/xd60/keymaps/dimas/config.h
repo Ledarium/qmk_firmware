b676e4e117a8680a9b11ff377ac5ddfe4416c1f4
Dmitry Maylarov <maylarov@yandex.ru>
2020-02-08 09:32:42 +0300

    add config

---
+++ keyboards/xd60/keymaps/dimas/config.h
@@ -0,0 +1,8 @@
+/* Delay for tap/hold for tap dance commands */
+#define TAPPING_TERM 180
+/* Disable RGB when host goes to sleep */
+#define RGBLIGHT_SLEEP
+
+#undef RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
