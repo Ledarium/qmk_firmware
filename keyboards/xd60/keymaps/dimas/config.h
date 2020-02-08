e8cbee2012cf3fe88bacf18e7dec03c96800e05f
Dmitry Maylarov <maylarov@yandex.ru>
2020-02-08 21:39:45 +0300

    add layer-based rgb rm animations

--- keyboards/xd60/keymaps/dimas/config.h
+++ keyboards/xd60/keymaps/dimas/config.h
@@ -1,8 +1,7 @@
 /* Delay for tap/hold for tap dance commands */
 #define TAPPING_TERM 180
+
 /* Disable RGB when host goes to sleep */
 #define RGBLIGHT_SLEEP
 
 #undef RGBLIGHT_ANIMATIONS
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
--- keyboards/xd60/keymaps/dimas/keymap.c
+++ keyboards/xd60/keymaps/dimas/keymap.c
@@ -7,7 +7,7 @@
 #define _ KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[DEFAULT] = LAYOUT_all(
+    [DEFAULT] = LAYOUT_all(
             KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_PSCR,
             KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
             LT(1,KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_ENT,
@@ -21,16 +21,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
             KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
             ),
-	[FKEYS] = LAYOUT_all(
+    [FKEYS] = LAYOUT_all(
             KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO,
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
             RESET, RESET, KC_NO, KC_NO, KC_NO, RGB_MOD, RGB_TOG, RGB_VAD, RGB_VAI, BL_TOGG, BL_DEC, BL_INC, KC_MUTE, KC_VOLU, KC_MPLY,
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT),
-	[NUMPAD] = LAYOUT_all(
+    [NUMPAD] = LAYOUT_all(
             KC_NLCK, KC_PSLS, KC_PAST, KC_PEQL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC, KC_NO,
             KC_PMNS, KC_P7, KC_P8, KC_P9, KC_BSPC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
             KC_PPLS, KC_P4, KC_P5, KC_P6, KC_BSPC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
             KC_PENT, KC_PENT, KC_P1, KC_P2, KC_P3, KC_PDOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
             KC_NO, KC_NO, KC_PDOT, KC_P0, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO)
 };
+
+void matrix_init_user() {
+    rgblight_setrgb(RGB_WHITE);
+    rgblight_enable();
+}
+
+uint32_t layer_state_set_user(uint32_t state) {
+    uint8_t layer = biton32(state);
+    switch (layer) {
+        case MOD:
+            rgblight_setrgb(RGB_ORANGE);
+            break;
+
+        case FKEYS:
+            rgblight_setrgb(RGB_RED);
+            break;
+
+        case NUMPAD:
+            rgblight_setrgb(RGB_CYAN);
+            break;
+
+        default:
+            rgblight_setrgb(RGB_WHITE);
+            break;
+    }
+    return state;
+}
