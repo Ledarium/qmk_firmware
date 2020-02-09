cf8081c900011f883237407b8d9fb07e88454a71
Dmitry Maylarov <maylarov@yandex.ru>
2020-02-09 16:43:16 +0300

    Ok bro I am sorry, Shift

--- keyboards/xd60/keymaps/dimas/keymap.c
+++ keyboards/xd60/keymaps/dimas/keymap.c
@@ -13,6 +13,7 @@ enum {
 
 enum {
     ESC_MOD = LT(MOD,KC_ESC),
+    SPC_MOD = LT(MOD,KC_SPC),
     LCTL_BS = MT(MOD_LCTL, KC_BSPC)
 };
 
@@ -21,8 +22,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_GRV, KC_1,   KC_2,   KC_3,  KC_4,  KC_5,   KC_6,   KC_7,    KC_8,   KC_9,    KC_0, KC_MINS, KC_EQL,  KC_BSPC, KC_PSCR,
             KC_TAB,   KC_Q,   KC_W,   KC_E,  KC_R,  KC_T,   KC_Y,   KC_U,    KC_I,   KC_O,    KC_P,  KC_LBRC,  KC_RBRC,      KC_BSLS,
             ESC_MOD,    KC_A,   KC_S,   KC_D,  KC_F,  KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT,        _, KC_ENT,
-            KC_BSPC, _,   KC_Z,   KC_X,   KC_C, KC_V,   KC_B,   KC_N,   KC_M,  KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC, KC_UP,   KC_DEL,
-            LCTL_BS, KC_LGUI, KC_LALT,                   KC_SPC,                     TG(NUMPAD),OSL(FKEYS),KC_LEFT, KC_DOWN, KC_RGHT
+            KC_LSPO, _,   KC_Z,   KC_X,   KC_C, KC_V,   KC_B,   KC_N,   KC_M,  KC_COMM,  KC_DOT, KC_SLSH,  KC_RSPC, KC_UP,   KC_DEL,
+            LCTL_BS, KC_LGUI, KC_LALT,                   SPC_MOD,                    TG(NUMPAD),OSL(FKEYS),KC_LEFT, KC_DOWN, KC_RGHT
             ),
     [GAME] = LAYOUT_all(
             KC_GRV, KC_1,   KC_2,   KC_3,  KC_4,  KC_5,   KC_6,   KC_7,    KC_8,   KC_9,    KC_0, KC_MINS, KC_EQL,  KC_BSPC, KC_PSCR,
@@ -39,11 +40,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TRNS, KC_TRNS, KC_TRNS,                   KC_BSPC,                                                ____,       ____,   KC_HOME, KC_PGDN, KC_END
             ),
     [FKEYS] = LAYOUT_all(
-            ____,    KC_F1,   KC_F2,   KC_F3,       KC_F4,      KC_F5,   KC_F6,      KC_F7,      KC_F8,    KC_F9,   KC_F10,     KC_F11, KC_F12,  ____,    ____,
-            ____,    ____,    ____,    ____,        ____,       ____,    ____,       ____,       ____,     ____,    ____,       ____,   ____,    ____,
-            ____,    KC_ASDN, KC_ASUP, KC_ASRP,     ____,       ____,    ____,       ____,       ____,     ____,    ____,       ____,   ____,    ____,
-            RESET, _,  ____,    ____,  ____,        RGB_MOD,    RGB_TOG, RGB_VAD,    RGB_VAI,  BL_TOGG, BL_DEC,     BL_INC, KC_MUTE, KC_VOLU, KC_MPLY,
-            ____,    ____,    ____,                    TG(GAME),                                ____,       KC_TRNS, KC_MPRV,    KC_VOLD,    KC_MNXT),
+            ____,    KC_F1,   KC_F2,   KC_F3,    KC_F4,      KC_F5,   KC_F6,      KC_F7,      KC_F8,    KC_F9,   KC_F10,     KC_F11, KC_F12,  ____,    ____,
+            ____,    ____,    ____,    ____,     ____,       ____,    ____,       ____,       ____,     ____,    ____,       ____,   ____,    ____,
+            ____,    KC_ASDN,    ____, ____,     ____,       ____,    ____,       ____,       ____,     ____,    ____,       ____,   ____,    ____,
+            RESET, _,  ____,    ____,  ____,     RGB_MOD,    RGB_TOG, RGB_VAD,    RGB_VAI,  BL_TOGG, BL_DEC,     BL_INC, KC_MUTE, KC_VOLU, KC_MPLY,
+            ____,    ____,    ____,                 TG(GAME),                                ____,       KC_TRNS, KC_MPRV,    KC_VOLD,    KC_MNXT),
     [NUMPAD] = LAYOUT_all(
             KC_NLCK, KC_PSLS, KC_PAST, KC_PEQL,     ____,       ____,    ____,       ____,       ____,     ____,    ____,       ____,   ____,    KC_BSPC, ____,
             KC_PMNS, KC_P7,   KC_P8,   KC_P9,       KC_BSPC,    ____,    ____,       ____,       ____,     ____,    ____,       ____,   ____,    ____,
