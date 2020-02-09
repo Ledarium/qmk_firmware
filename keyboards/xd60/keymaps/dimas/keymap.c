1889af022dd2084ff9eb3c11dc2afa20598e0089
Dmitry Maylarov <maylarov@yandex.ru>
2020-02-09 23:44:37 +0300

    Deleted "compose"

--- keyboards/xd60/keymaps/dimas/keymap.c
+++ keyboards/xd60/keymaps/dimas/keymap.c
@@ -16,13 +16,12 @@ enum {
 enum {
     ESC_MOD = LT(MOD,KC_ESC),
     SPC_MOD = LT(MOD,KC_SPC),
-    COM_PSC = MT(MOD_RSFT | MOD_RALT, KC_PSCR),
     LCTL_BS = MT(MOD_LCTL, KC_BSPC)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DEFAULT] = LAYOUT_all(
-            KC_GRV, KC_1,    KC_2,   KC_3,  KC_4,  KC_5,   KC_6,   KC_7,    KC_8,   KC_9,    KC_0, KC_MINS, KC_EQL,  KC_BSPC,  COM_PSC,
+            KC_GRV, KC_1,    KC_2,   KC_3,  KC_4,  KC_5,   KC_6,   KC_7,    KC_8,   KC_9,    KC_0, KC_MINS, KC_EQL,  KC_BSPC,  KC_PSCR,
             KC_TAB,    KC_Q,   KC_W,   KC_E,  KC_R,  KC_T,   KC_Y,   KC_U,    KC_I,   KC_O,    KC_P,  KC_LBRC,  KC_RBRC,       KC_BSLS,
             ESC_MOD,      KC_A,   KC_S,   KC_D,  KC_F,  KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT,        _, KC_ENT,
             KC_LSPO, _,      KC_Z,   KC_X,   KC_C, KC_V,   KC_B,   KC_N,   KC_M,  KC_COMM,  KC_DOT, KC_SLSH,  KC_RSPC, KC_UP,  KC_DEL,
