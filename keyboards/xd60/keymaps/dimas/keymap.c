826a7a2657a7e28494b259bedb16b5926b4a3a2f
Dmitry Maylarov <maylarov@yandex.ru>
2020-04-08 19:40:42 +0300

    optimize

--- keyboards/xd60/keymaps/dimas/keymap.c
+++ keyboards/xd60/keymaps/dimas/keymap.c
@@ -53,18 +53,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                     TG(NUMPAD),OSL(FKEYS),KC_LEFT, KC_DOWN, KC_RGHT
             ),
     [MOD1] = LAYOUT_all(
-            ____,     ____,  ____,  ____,  ____,  ____,   ____,     ____,    ____,    ____,    ____,   ____,  ____, ____,   ____,
+            ____,    KC_F1,  KC_F2,  KC_F3,    KC_F4,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11, KC_F12,  ____,    ____,
             KC_DOT, KC_1,   KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   MACRO_1_8, ____, ____,
             KC_BSPC,  KC_LBRC, KC_RBRC, KC_AT,  KC_HASH, KC_AMPR, KC_EXLM, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT,  KC_DLR, _,    ____,
             KC_TRNS,_, KC_LCBR, KC_RCBR, KC_DLR, KC_PERC, KC_CIRC, KC_EQL,  KC_UNDS, KC_PLUS, KC_MINS, KC_ASTR, ____, KC_PGUP, ____,
             KC_TRNS, KC_TRNS, KC_TRNS,                 KC_TRNS,                               ____, ____, KC_HOME, KC_PGDN, KC_END
             ),
     [MOD2] = LAYOUT_all(
-            ____,    ____,    ____,    LALT(KC_F4), KC_END,     ____, ____,    ____,    ____,  ____,    KC_HOME, ____, ____, ____,    KC_PAUS,
-            KC_ESC,  ____,    ____,    ____,        ____,       ____, ____,    ____,    ____,  ____,    ____,    ____, ____, ____,
-            KC_TRNS, KC_PAST, KC_PPLS, LCTL(KC_X),  LCTL(KC_F), ____, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, ____,    ____, _,    ____,
-            KC_TRNS, _,    ____, ____,     ____,    ____,    ____,    ____,    ____,  ____,    ____,    ____, ____, KC_PGUP, ____,
-            KC_TRNS, KC_TRNS, KC_TRNS,              KC_SPC,                                    ____, ____, KC_HOME, KC_PGDN, KC_END
+            ____,    ____,    KC_F2,    LALT(KC_F4), KC_END,     ____, ____,    ____,    ____,    ____,   KC_HOME, ____, ____, ____,    KC_PAUS,
+            KC_ESC,  ____,    ____,    ____,        ____,       ____, ____,    ____,    ____,    ____,    ____,    ____, ____, ____,
+            KC_TRNS, KC_PAST, KC_PPLS, LCTL(KC_X),  LCTL(KC_F), ____, ____, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  ____,    _,    ____,
+            KC_TRNS, _,    ____, ____,     ____,    ____,    ____,    ____,    ____,  ____,      ____,    ____, ____, KC_PGUP, ____,
+            KC_TRNS, KC_TRNS, KC_TRNS,              KC_SPC,                                      ____, ____, KC_HOME, KC_PGDN, KC_END
             ),
     [FKEYS] = LAYOUT_all(
             ____,    KC_F1,  KC_F2,  KC_F3,    KC_F4,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11, KC_F12,  ____,    ____,
